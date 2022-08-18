600f5f89e7d4e815f37ac7600bf86c7e55c803a1
Nishanth <nishanthdass@gmail.com>
2022-08-18 09:26:03 -0400

    connection to google api formed for testing

--- client/src/forms.jsx
+++ client/src/forms.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useRef, useState } from 'react';
 import ResultText from './textarea'
 import './App.css'
 
@@ -6,30 +6,36 @@ import './App.css'
 export default function IsbnForm(props) {
   const [authorVal, setAuthor] = useState('');
   const [bookVal, setBook] = useState('');
+  
 
   const handleSubmit = (e) => {
     e.preventDefault()
-    fetch('http://localhost:3001/api/people')
-    .then((response) => response.json())
-    .then((data) => show(data));
-
-    // console.log({bookVal})
-    // console.log({authorVal})
     const author = JSON.stringify(authorVal)
     const book = JSON.stringify(bookVal)
-    console.log(typeof book, book)
+  if (author.length > 2 || book.length > 2) {
+    console.log(author.length)
+    fetch('http://localhost:3001/api/people',  {  
+      method: 'post',
+      headers: {'Content-Type': 'application/json'},
+      body: JSON.stringify({
+        "author" : author,
+        "book" : book
+      }),
+  })
+      // .then(res => res.json())
+      // .then(data => console.log(data))
+  }
     function show(data){
-      console.log(data)
-      document.getElementById('resultTextarea').innerHTML += "FrontEnd Says: The book you want to cite is " + book + " and the author is " + author + ". BackEnd Says: " + data[0].name
+      // document.getElementById('resultTextarea').innerHTML += "FrontEnd Says: The book you want to cite is " + book + " and the author is " + author + ". BackEnd Says: The book you want to cite is " + data.book +  " and the author is " + data.author + "."
+      document.getElementById('resultTextarea').innerHTML += data
 
     }
-
-    setAuthor('')
-    setBook('')
+    // setAuthor('')
+    // setBook('')
   }
 
   return (
-        <form onSubmit={handleSubmit}>
+        <form onChange={handleSubmit}>
         <label>Author Name Request:</label>
         <br />
         <input 
