4b824d024b98c1ba89d1c6fc3ae65b43eecb4270
Nishanth <nishanthdass@gmail.com>
2022-08-22 20:42:27 -0400

    getting search and adding style

--- client/src/formatbtn.jsx
+++ client/src/formatbtn.jsx
@@ -5,7 +5,7 @@ import './App.css'
 export default function FormatBtn(props) {
   return (
     <>
-    <select className='select-format'> 
+    <select className='select-format' id="select-format-id"> 
     <option value="mla">MLA</option>
     <option value="apa">APA</option>
     <option value="chicago">Chicago</option>
